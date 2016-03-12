3e971ad22ec1b87b72c52ebb59a46af4e5274b51
MichellR14 <delfina.recarte@ujcv.edu.hn>
2016-03-11 22:34:00 -0600

    avance1

---
+++ Proyecto/app/src/main/java/com/example/michell/importar_img/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.michell.importar_img;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private ImageLoader mImageLoader;
+    private static MainActivity mInstance;
+    private static Context mAppContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mInstance=this;
+        this.setAppContext(getApplicationContext());
+
+        mImageLoader = VolleySingleton.getInstance().getImageLoader();
+        NetworkImageView avatar = (NetworkImageView) findViewById(R.id.imagen);
+        avatar.setImageUrl("https://www-kiva-org-1.global.ssl.fastly.net/img/w800/dbaaf9f617048702cce9d9d4be22506c.jpg",mImageLoader);
+
+    }
+
+    public static MainActivity getInstance(){
+        return mInstance;
+    }
+    public static Context getAppContext() {
+        return mAppContext;
+    }
+    public void setAppContext(Context mAppContext) {
+        this.mAppContext = mAppContext;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
